19c76ae688ebe9a1473251e68b1cd551c61a8988
Daniel Galán <danielgalanrodri@gmail.com>
2024-12-01 11:51:28 +0100

    Add common utility function to read file

---
+++ utils.js
@@ -0,0 +1,6 @@
+exports.readFile = (path) => {
+  const fs = require('fs')
+  const file = fs.readFileSync(path, { encoding: 'utf8' })
+  const fileByLine = file.split(/\r?\n/)
+  return fileByLine
+}
