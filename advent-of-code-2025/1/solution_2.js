0517e459a56d9c944277d387c045f1cb19f1d144
Daniel Galán <danielgalanrodri@gmail.com>
2025-12-02 23:41:09 +0100

    2025: Solution day 1-2

--- advent-of-code-2025/1/solution_2.js
+++ advent-of-code-2025/1/solution_2.js
@@ -1,10 +1,10 @@
 const { readFile } = require("../../utils")
 
-const instructions = readFile('./example_data.txt')
+const instructions = readFile('./puzzle.txt')
 
 
 let start = 50
-let password = []
+let password = 0
 
 
 function parseStep(step) {
@@ -14,32 +14,48 @@ function parseStep(step) {
     return { direction, distance: parseInt(distance) }
 }
 
-
-instructions.forEach(step => {
+// Starts NOT 0 ends NOT 0
+// Starts 0 ends NOT 0
+// Starts NOT 0 ends 0
+instructions.forEach((step, i) => {
     const { direction, distance } = parseStep(step)
     const numberOfLaps = Math.trunc(distance / 100)
-    numberOfLaps > 0 && password.push(numberOfLaps)
+    password += numberOfLaps
 
-    let newPosition = direction == 'L' ?
-        (start - distance) % 100 :
-        (start + distance) % 100
+    // The key is to work with the remaining distance
+    const remainingDistance = distance % 100
+
+    let endPosition = direction == 'L' ?
+        (start - remainingDistance) :
+        (start + remainingDistance)
+
+    // Position correction if it falls beyond the dial limits
+    if (endPosition < 0) endPosition += 100
+    if (endPosition > 99) endPosition -= 100
+
 
     if (direction == 'L') {
-        if (newPosition < 0) {
-            newPosition += 100;
-            start != 0 && password.push(1)
+        // Has crossed 0:
+        // Ex: Starts 10, finishes 90. Path 10,9,8,...,0,...,92,91,90
+        if (endPosition > start) {
+            // Don't count if started at 0
+            // Ex: Started 0 Finishes 90. Has not crossed, it started from 0.
+            start != 0 && (password += 1)
         }
     } else {
-        if (newPosition != 0 && start != 0 && start > newPosition) password.push(1)
+        // Has crossed 0:
+        // Ex: Starts 90, finishes 10. Path 91,92,93,...,0,...,8,9,10
+        // Don't count if ends at 0
+        // Ex: Starts 90 ends 0. Has not crossed, it has landed on 0.
+        if (endPosition != 0 && endPosition < start) { password += 1 }
     }
 
+    // Has Landed on 0
+    // Don't count if started at 0
+    // Ex: Starts 0 ends 0. It's just one full lap. Already counted
+    if (start != 0 && endPosition == 0) password += 1
 
-    if (newPosition == 0) password.push(1)
-
-    start = newPosition
+    start = endPosition
 });
-const result = password.reduce((acc, value) => acc + value, 0)
-console.log('password: ', password.length);
-console.log('password', result);
 
-// 6288 --> too hight
+console.log('password', password);
